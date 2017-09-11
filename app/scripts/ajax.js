5531ac4247b7061e19ec94ac71c1892079b2c4b6
Quang Vu <vhquang@gmail.com>
2017-09-10 19:36:44 -0700

    implement an ajax call

--- app/scripts/ajax.js
+++ app/scripts/ajax.js
@@ -1,14 +1,15 @@
 /**
- * Demo of XMLHttpRequest AJAX, for more info:
+ * A bare XMLHttpRequest AJAX, for more info:
  * https://developer.mozilla.org/en-US/docs/AJAX/Getting_Started
  */
-function ajaxDemo() {
-    var url = 'https://api.github.com/repos/pandas-dev/pandas/pulls?per_page=1';
+function ajax(url, success) {
     var httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = function() {
         if (httpRequest.readyState === XMLHttpRequest.DONE) {
-            var json = JSON.parse(httpRequest.responseText);
-            outputJson('Ajax output', json);
+            if (httpRequest.status === 200) {
+                var json = JSON.parse(httpRequest.responseText);
+                success(json);
+            }
         }
     };
     httpRequest.open('GET', url, true);

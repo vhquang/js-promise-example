48dfb7110faa8184b1497cf7a3255690901ad09a
Quang Vu <vu@rallc.com>
2017-09-12 12:20:52 -0700

    refactor callback for better visual

--- app/scripts/callback1.js
+++ app/scripts/callback1.js
@@ -1,12 +1,16 @@
 function getData() {
     var prUrl = 'https://api.github.com/repos/pandas-dev/pandas/pulls?per_page=3';
-    ajax(prUrl, function(data) {
+    ajax(prUrl, function success(data) {
         outputJson('Pull Request', data[0]);
-        ajax(data[0]['commits_url'], function(data) {
+
+        ajax(data[0]['commits_url'], function success(data) {
             outputJson('Commit', data[0]);
-            ajax(data[0]['author']['url'], function(data) {
+
+            ajax(data[0]['author']['url'], function success(data) {
                 outputJson('Author', data);
             });
+
         });
+
     });
 }
